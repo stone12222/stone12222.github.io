98172d2338c12f7313b77c162d12d54d021c6998
stone12222 <oggoggg1@gmail.com>
2023-01-17 20:38:41 -0500

    web update part2

---
+++ Website2022-main/README.md
@@ -0,0 +1,38 @@
+# Team 6854's Website!
+
+Welcome to **FRC 6854's** website repository. Here you can find the website files and the testing/live branch.
+
+
+# Folders
+
+The main folders you need to know about are: 
+- CSS
+- Images
+- JS
+- Media
+- Main Directory
+
+# Contents of Folders
+
+|Folders         |Whats Inside                   |
+|----------------|-------------------------------|
+|CSS             |CSS declarations for project with style.css|
+|Images          |Its pretty self-explanatory, the folder includes the images|
+|JS              |JS files with JQuery functions for reactive displays|
+|Media           |Media files that are not images, for example our RSS feed|
+|Main Directory  |HTML files with web server files like .htaccess|
+
+# Publication
+
+The website is open source and you are able to use the code on your own website.
+> Dont forget to credit **Team 6854** when you use this website.
+
+# Pull Requests
+
+Pull requests are accepted but you must first tell us why you are pulling the website.
+
+# License
+The project uses the MIT license so that means that the project is free of charge but the copyright notice must be included in all distributions.
+
+# Visit the Website
+[https://team6854.com](http://team6854.com)![Lucas Robotics Logo](https://team6854.com/images/valkyrie/icon.ico)
---
+++ Website2022-main/js/countdown.js
@@ -0,0 +1,48 @@
+//Countdown
+
+// Set the date we're counting down to
+var countDownDate = new Date("Mar 21, 2019 01:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+// Get todays date and time
+var now = new Date().getTime();
+
+// Find the distance between now and the count down date
+var distance = countDownDate - now;
+
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+if(days <= 0){
+    if(hours <= 0){
+        if(minutes <= 0){
+            if(seconds <= 0){
+                document.getElementById("time").innerHTML = "Competition has started!";
+                return;
+            }
+            document.getElementById("time").innerHTML = seconds + "s ";
+            return;
+        }
+        document.getElementById("time").innerHTML = minutes + "m " + seconds + "s ";
+        return;
+    }
+    document.getElementById("time").innerHTML = hours + "h "
+    + minutes + "m " + seconds + "s ";
+    return;
+}
+else{
+    document.getElementById("time").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+}
+
+// If the count down is finished, write some text 
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("time").innerHTML = "EXPIRED";
+}
+}, 1000);
